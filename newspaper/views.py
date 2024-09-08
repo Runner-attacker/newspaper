df7f10d8bcd2868c8c5595690006717dbecf095b
Sahil Koirala <koirala24sahil@gmail.com>
2024-09-08 20:31:54 +0545

    Initial Commit

---
+++ newspaper/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from newspaper.models import Post
+from django.views.generic import ListView
+
+# Create your views here.
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = "aznews/home.html"
+    context_object_name = "posts"
+
+    queryset = Post.objects.filter(
+        published_at__isnull=False,
+        status="active",
+    ).order_by("-published_at")[:5]
